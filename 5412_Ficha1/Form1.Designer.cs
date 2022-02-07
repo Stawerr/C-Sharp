b92bcf224a0d72078a23025b9025da283fac6a38
Stawerr <renato-silva_16@hotmail.com>
2022-02-07 22:43:53 +0000

    Add project files.

---
+++ 5412_Ficha1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace _5412_Ficha1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.maisA = new System.Windows.Forms.Button();
+            this.menosA = new System.Windows.Forms.Button();
+            this.maisB = new System.Windows.Forms.Button();
+            this.menosB = new System.Windows.Forms.Button();
+            this.labelTempo = new System.Windows.Forms.Label();
+            this.equipaA = new System.Windows.Forms.Label();
+            this.equipaB = new System.Windows.Forms.Label();
+            this.ScoreA = new System.Windows.Forms.Label();
+            this.ScoreB = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.parte = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // maisA
+            // 
+            this.maisA.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maisA.Location = new System.Drawing.Point(45, 360);
+            this.maisA.Name = "maisA";
+            this.maisA.Size = new System.Drawing.Size(47, 36);
+            this.maisA.TabIndex = 0;
+            this.maisA.Text = "+";
+            this.maisA.UseVisualStyleBackColor = true;
+            this.maisA.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menosA
+            // 
+            this.menosA.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menosA.Location = new System.Drawing.Point(112, 362);
+            this.menosA.Name = "menosA";
+            this.menosA.Size = new System.Drawing.Size(44, 33);
+            this.menosA.TabIndex = 1;
+            this.menosA.Text = "-";
+            this.menosA.UseVisualStyleBackColor = true;
+            this.menosA.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // maisB
+            // 
+            this.maisB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maisB.Location = new System.Drawing.Point(444, 355);
+            this.maisB.Name = "maisB";
+            this.maisB.Size = new System.Drawing.Size(52, 40);
+            this.maisB.TabIndex = 2;
+            this.maisB.Text = "+";
+            this.maisB.UseVisualStyleBackColor = true;
+            this.maisB.Click += new System.EventHandler(this.cliqueAqui);
+            // 
+            // menosB
+            // 
+            this.menosB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menosB.Location = new System.Drawing.Point(516, 355);
+            this.menosB.Name = "menosB";
+            this.menosB.Size = new System.Drawing.Size(52, 40);
+            this.menosB.TabIndex = 4;
+            this.menosB.Text = "-";
+            this.menosB.UseVisualStyleBackColor = true;
+            this.menosB.Click += new System.EventHandler(this.menosB_Click);
+            // 
+            // labelTempo
+            // 
+            this.labelTempo.AutoSize = true;
+            this.labelTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTempo.Location = new System.Drawing.Point(241, 101);
+            this.labelTempo.Name = "labelTempo";
+            this.labelTempo.Size = new System.Drawing.Size(151, 59);
+            this.labelTempo.TabIndex = 7;
+            this.labelTempo.Text = "00:00";
+            // 
+            // equipaA
+            // 
+            this.equipaA.AutoSize = true;
+            this.equipaA.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equipaA.Location = new System.Drawing.Point(63, 263);
+            this.equipaA.Name = "equipaA";
+            this.equipaA.Size = new System.Drawing.Size(98, 29);
+            this.equipaA.TabIndex = 8;
+            this.equipaA.Text = "Benfica";
+            this.equipaA.Click += new System.EventHandler(this.equipaA_Click);
+            // 
+            // equipaB
+            // 
+            this.equipaB.AutoSize = true;
+            this.equipaB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equipaB.Location = new System.Drawing.Point(472, 263);
+            this.equipaB.Name = "equipaB";
+            this.equipaB.Size = new System.Drawing.Size(73, 29);
+            this.equipaB.TabIndex = 9;
+            this.equipaB.Text = "Porto";
+            // 
+            // ScoreA
+            // 
+            this.ScoreA.AutoSize = true;
+            this.ScoreA.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreA.Location = new System.Drawing.Point(93, 313);
+            this.ScoreA.Name = "ScoreA";
+            this.ScoreA.Size = new System.Drawing.Size(27, 29);
+            this.ScoreA.TabIndex = 10;
+            this.ScoreA.Text = "0";
+            // 
+            // ScoreB
+            // 
+            this.ScoreB.AutoSize = true;
+            this.ScoreB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreB.Location = new System.Drawing.Point(495, 313);
+            this.ScoreB.Name = "ScoreB";
+            this.ScoreB.Size = new System.Drawing.Size(27, 29);
+            this.ScoreB.TabIndex = 11;
+            this.ScoreB.Text = "0";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(421, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(182, 171);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 171);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // parte
+            // 
+            this.parte.AutoSize = true;
+            this.parte.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.parte.Location = new System.Drawing.Point(264, 180);
+            this.parte.Name = "parte";
+            this.parte.Size = new System.Drawing.Size(100, 29);
+            this.parte.TabIndex = 12;
+            this.parte.Text = "1ª parte";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(669, 450);
+            this.Controls.Add(this.parte);
+            this.Controls.Add(this.ScoreB);
+            this.Controls.Add(this.ScoreA);
+            this.Controls.Add(this.equipaB);
+            this.Controls.Add(this.equipaA);
+            this.Controls.Add(this.labelTempo);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menosB);
+            this.Controls.Add(this.maisB);
+            this.Controls.Add(this.menosA);
+            this.Controls.Add(this.maisA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button maisA;
+        private System.Windows.Forms.Button menosA;
+        private System.Windows.Forms.Button maisB;
+        private System.Windows.Forms.Button menosB;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label labelTempo;
+        private System.Windows.Forms.Label equipaA;
+        private System.Windows.Forms.Label equipaB;
+        private System.Windows.Forms.Label ScoreA;
+        private System.Windows.Forms.Label ScoreB;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label parte;
+    }
+}
+
---
+++ 5412_Ficha1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5412_Ficha1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            selecionaImagem(equipaA, pictureBox1);
+            selecionaImagem(equipaB, pictureBox2);
+
+        }
+
+        private void selecionaImagem(Label equipa, PictureBox pictureBox)
+        {
+            if (equipa.Text == "Arouca")
+            {
+                pictureBox.Image = Properties.Resources.Arouca;
+            }
+            if (equipa.Text == "Benfica")
+            {
+                pictureBox.Image = Properties.Resources.Benfica;
+            }
+            if (equipa.Text == "Famalicao")
+            {
+                pictureBox.Image = Properties.Resources.Famalicao;
+            }
+            if (equipa.Text == "Porto")
+            {
+                pictureBox.Image = Properties.Resources.Porto;
+            }
+            if (equipa.Text == "Sporting")
+            {
+                pictureBox.Image = Properties.Resources.Sporting;
+            }
+            if (equipa.Text == "Tondela")
+            {
+                pictureBox.Image = Properties.Resources.Tondela;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ScoreA.Text = (int.Parse(ScoreA.Text) + 1).ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int texto = int.Parse(ScoreA.Text);
+            if (texto > 0){
+                ScoreA.Text = (texto - 1).ToString();
+            }
+
+        }
+
+        private void cliqueAqui(object sender, EventArgs e)
+        {
+            ScoreB.Text = (int.Parse(ScoreB.Text) + 1).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+            int min = 0;
+            int seg = 0;
+            private void timer1_Tick(object sender, EventArgs e)
+                { if (min < 90)
+                {
+                    if (seg < 59)
+                    {
+                        seg++; 
+                    }
+                    else if(seg == 59)
+                        { 
+                        seg = 0;
+                        min++; 
+                    }
+                } labelTempo.Text = min + ":" + seg;
+
+            if (min==45 && seg==0)
+            {
+                parte.Text = "2ª parte";
+            }
+        }
+
+            
+
+
+        private void menosB_Click(object sender, EventArgs e)
+        {
+            int texto = int.Parse(ScoreB.Text);
+            if (texto > 0)
+            {
+                ScoreB.Text = (texto - 1).ToString();
+            }
+        }
+
+        private void equipaA_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
